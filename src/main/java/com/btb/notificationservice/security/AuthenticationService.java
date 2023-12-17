8ffa644480525c582f277e4245caac393aa21b88
Edgar <edouton@uoc.edu>
2023-12-17 21:00:42 +0100

    Add api key

--- src/main/java/com/btb/notificationservice/security/AuthenticationService.java
+++ src/main/java/com/btb/notificationservice/security/AuthenticationService.java
@@ -9,7 +9,7 @@ public class AuthenticationService {
 
     private static final String AUTH_TOKEN_HEADER_NAME = "BTB-API-KEY";
 
-    private static final String AUTH_TOKEN = "A7243431-AD97-4392-A244-A16702429927";
+    private static final String AUTH_TOKEN = "BA2CE40F-D832-47ED-A183-A8103B68CD09";
 
     public static Authentication getAuthentication(HttpServletRequest request) {
         String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
