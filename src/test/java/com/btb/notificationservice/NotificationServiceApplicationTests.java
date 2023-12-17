ba140fc287cb7bfa8c9fd889b2637e5cdbdab242
Edgar Doutón <38222402+lEdgaar@users.noreply.github.com>
2023-12-17 17:06:16 +0100

    Spring initializr

---
+++ src/test/java/com/btb/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btb.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
